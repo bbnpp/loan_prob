8dd9045d55186ca7deb543099a9aec828e08fd52
bb426 <stacker426>
2021-08-03 01:14:55 +0900

    add transformers

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 
 def has_pk(data:pd.DataFrame):
@@ -21,14 +22,32 @@ def search_categoric(data:pd.DataFrame, cardinality_limit=10):
     return [col for col in data if data[col].nunique() < cardinality_limit]
 
 
-def aggregate_numeric(data:pd.DataFrame, primary_key, drop_cols):
-    aggregated = (data.drop(drop_cols, axis=1)
-                      .groupby(primary_key)
+def aggregate_numerical(data:pd.DataFrame, drop_cols):
+    aggregated = (data.select_dtypes(exclude='object')
+                      .drop(drop_cols, axis=1)
+                      .groupby(level=0)
                       .agg([np.nanmean, np.nanstd, len]))
     aggregated.columns = ['_'.join(col).strip() for col in aggregated.columns.values]
     return aggregated
 
 
-# def aggregate_categoric
+def aggregate_categorical(data:pd.DataFrame):
+    aggregated = (data.select_dtypes(include='object')
+                      .groupby(level=0)
+                      .agg([min, max])
+                 )
+    aggregated.columns = ['_'.join(col).strip() for col in aggregated.columns.values]
+    return aggregated
 
+
+def aggregate(data:pd.DataFrame, primary_key, drop_cols):
+    data.set_index(primary_key, inplace=True)
+    numerical = aggregate_numerical(data, drop_cols)
+    
+    categorical_columns = data.select_dtypes(include='object').columns
+    if len(categorical_columns) > 0:
+        categorical = aggregate_categorical(data)
+        return pd.concat([numerical, categorical], axis=1, join='inner')
+    else:
+        return numerical
     
